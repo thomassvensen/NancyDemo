c136839e53ff1e773090fb43e93c27a78ff3b788
thomassvensen <thomas@sandsvensen.com>
2013-11-03 22:47:41 +0100

    Initial

---
+++ HelloILanTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nancy.Testing;
+using NancyForIlan;
+
+namespace HelloILanTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            var browser = new Browser(c => c.Module<HelloModule>());
+
+            // act
+            var response = browser.Get("/hus/43", with =>
+            {
+                with.HttpRequest();
+                with.Header("accept", "application/json");
+            });
+
+            // assert
+            Assert.AreEqual("[\"hus\",\"43\"]", response.Body.AsString());
+
+        }
+    }
+}
---
+++ NancyForIlan/HelloModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyForIlan
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = _ => "Hello Ilan!";
+
+            Get["/{entity}/{id}"] = parameters =>
+                Negotiate.WithStatusCode(HttpStatusCode.OK)
+                    .WithModel(new[] {parameters.entity, parameters.id});
+        }
+    }
+}
